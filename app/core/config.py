0df72f9ed380a245217160220aaf9983e58b1f6e
Misael Vilchez <mvilchezmo@unsa.edu.pe>
2026-07-21 16:00:09 -0500

    refactor: change required environment variables

--- app/core/config.py
+++ app/core/config.py
@@ -1,15 +1,27 @@
-from pydantic import SecretStr
+from pydantic import SecretStr, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
+    database_protocol_driver: str
+    database_host: str
+    database_port: int
     database_user: str
     database_password: SecretStr
     database_name: str
 
-    database_connection_url: SecretStr
+    @computed_field
+    @property
+    def database_connection_url(self) -> SecretStr:
+        connection_url = (
+            f"{self.database_protocol_driver}://"
+            f"{self.database_user}:{self.database_password.get_secret_value()}@"
+            f"{self.database_host}:{self.database_port}/"
+            f"{self.database_name}"
+        )
+        return SecretStr(connection_url)
 
 
 settings = Settings()  # pyright: ignore[reportCallIssue]
